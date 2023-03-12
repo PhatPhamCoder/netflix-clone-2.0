3ba9e74d6d1b26384ec616ec7441a137b549cea8
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-13 00:48:32 +0700

    finish

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useCallback } from "react";
 import useBillBorad from "@/hooks/useBillboard";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import PlayButton from "./PlayButton";
+import useInfoModal from "@/hooks/useInfoModal";
 
 const Billboard = () => {
   const { data } = useBillBorad();
+  const { openModal } = useInfoModal();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
   return (
     <div className="relative h-[56.25vw]">
       <video
@@ -32,7 +39,11 @@ const Billboard = () => {
           {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
-          <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg flex flex-row items-center hover:bg-opacity-20 transition">
+          <PlayButton movieId={data?.id} />
+          <button
+            onClick={handleOpenModal}
+            className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg flex flex-row items-center hover:bg-opacity-20 transition"
+          >
             <AiOutlineInfoCircle className="mr-2" />
             More Infor
           </button>
